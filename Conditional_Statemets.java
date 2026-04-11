8f60dd282207c54fbded68f5b205acb548dda696
OM BURSE <omburse0019@gmail.com>
2026-04-11 19:20:01 +0530

    Conditional Statements

---
+++ Conditional_Statemets.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+public class Conditional_Statemets {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            /*
+            //Question 1:Write a Java program to get a number from the user and print whether it is positive or negative.
+            System.out.print("Enter a number: ");
+            int num = sc.nextInt();
+
+            if (num > 0) {
+                System.out.println("The number is positive.");
+            } else if (num < 0) {
+                System.out.println("The number is negative.");
+            } else {
+                System.out.println("The number is zero.");
+            }
+            sc.close();
+             */
+
+            //Question2:Finish the following code so that it prints You have a fever if your temperature is above 100 and
+            //otherwise prints You don't have a fever
+                /*
+                double temp = 103.5;
+                if(temp>100){
+                    System.out.println("You have a fever if your temperature");
+                }
+                else {
+                    System.out.println("You don't have a fever");
+                }
+                 */
+             // Question3:Write a Java program to input week number(1-7)and print day of week name using switch case.
+                /*
+                System.out.print("Enter The Day Number With Range 1 to 7 : ");
+                int Day = sc.nextInt();
+                switch (Day){
+                    case 1 :
+                        System.out.println("Monday");
+                        break;
+                    case 2 :
+                        System.out.println("Tuesday");
+                        break;
+                    case 3 :
+                        System.out.println("Wednesday");
+                        break;
+                    case 4 :
+                        System.out.println("Thursday");
+                        break;
+                    case 5 :
+                        System.out.println("Friday");
+                        break;
+                    case 6 :
+                        System.out.println("Saturday");
+                        break;
+                    case 7 :
+                        System.out.println("Sunday");
+                        break;
+                    default:
+                        System.out.println("Invalid Number , Sorry");
+                }
+                 */
+
+        
+
+
+
+
+
+        }
+
+}
