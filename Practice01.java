e590c2ca743e5d076dbf1e27ef53f990a01f525d
OM BURSE <omburse0019@gmail.com>
2026-04-11 14:35:46 +0530

    Practice In Variable , Data Type and Input for U

---
+++ Practice01.java
@@ -0,0 +1,48 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Practice01 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        //Question1:In a program , input 3 numbers:A,B and C.You have to output the average of these 3 numbers.
+        /*
+        int a =10;
+        int b = 22;
+        int c = 33;
+        int Avg = a+b+c;
+        System.out.println("The three No of Avg is : " + (Avg)/3);
+         */
+
+
+        //Question 2:In a program,input the side of a square.
+        //You have to output the area of the square.
+        /*
+        System.out.print("Enter the Side of a Square are : ");
+        int side = sc.nextInt();
+        float side1 = (float) (side*side);
+        System.out.print("The Area Of The Square are : " + side1);
+         */
+        
+
+        //Question3:Enter cost of 3 items from the user (using float datatype)- A pencil ,A pen and an eraser.
+        // You have to output the total cost of the items back to the user as their bill.
+        /*
+        System.out.print("Enter the total Pencil : ");
+        float a = sc.nextFloat();
+
+        System.out.print("Enter the total Pen : ");
+        float b = sc.nextFloat();
+
+        System.out.print("Enter the total Eraser : ");
+        float c = sc.nextFloat();
+
+        int Bill = (int)(a+b+c);
+        float gst = Bill*18/100;
+
+        System.out.println("The total Bill are : " + Bill);
+        System.out.print("The 18% GST are : " + gst);
+
+         */
+
+
+    }
+}
