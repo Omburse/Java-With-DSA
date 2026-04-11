96ceda35809a5519e2f79b665f130147fa385ede
OM BURSE <omburse0019@gmail.com>
2026-04-11 11:45:30 +0530

    02_Datatype in Java

---
+++ DataType_02.java
@@ -0,0 +1,29 @@
+public class DataType_02 {
+    static void main() {
+        /*
+            Data Type in java
+            primitive data type :-
+            Byte
+            long
+            int
+            float
+            boolean
+            double
+            char
+            short
+
+            Non-Primitive Data in Java
+            String
+            Class
+            Object
+            Interface
+            Array
+         */
+        // Sum of A and B
+        byte a = 2;
+        byte b = 3;
+        int sum = a + b;
+        System.out.println(sum);
+
+    }
+}
