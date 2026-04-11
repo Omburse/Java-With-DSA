80b3273dc19027720847862ca0baad81bc00dd20
OM BURSE <omburse0019@gmail.com>
2026-04-11 11:44:58 +0530

    01_Variable In Java

---
+++ Variable_01.java
@@ -0,0 +1,26 @@
+public class Variable_01 {
+    static void main(String[]args) {
+        /*
+            Print * * * *
+                  * * *
+                  * *
+                  *
+            Output in Java
+         */
+        /*
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+
+         */
+        // Variables In java
+        // A and B are Variable they are stored values
+        int a = 10;
+        int b = 5;
+        int Ans = 2*(a+b);
+        System.out.println(Ans);
+        String Name  = "Om Ulhas Burse";
+        System.out.println("Your Name is : " + Name);
+    }
+}
