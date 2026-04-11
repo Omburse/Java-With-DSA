0724753f57144cafa3665faaae376b06f99cfa36
OM BURSE <omburse0019@gmail.com>
2026-04-11 19:19:09 +0530

    Operators In Java

---
+++ Operators_In_Java.java
@@ -0,0 +1,50 @@
+public class Operators_In_Java {
+    public static void main(String[]args){
+        // --Arithmetic Operator--
+        int A = 10;
+        int B = 5;
+        /*
+        //Add
+        System.out.println("The Add is : "+ A+B);
+        //Sub
+        //System.out.println("The Sub is : 20" -B);
+        //Multi
+        System.out.println("The Multi is : " + A*B);
+        //Div
+        System.out.println("The Div is " + A/B);
+        //Mod
+        System.out.println("The Mod is " + A%B);
+         */
+
+        //--Relational Operators--
+        //Equal
+        /*
+        System.out.println(A==B);
+        //Not Equal To
+        System.out.println(A!=B);
+        //Grater Than
+        System.out.println(A>B);
+        //Less Than
+        System.out.println(A<B);
+        //Grater Then Equal To
+        System.out.println(A>=B);
+        //Less Then Equal To
+        System.out.println(A<=B);
+         */
+
+
+        //--Logical Operators--
+        // && T and T = T otherwise F
+        // || F and F = F otherwise T
+        // !  Not means False asel tr True nhi tr ulta
+
+
+        //--Assignment Operators
+
+
+
+
+
+
+    }
+}
