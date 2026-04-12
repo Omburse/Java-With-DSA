eb8862b353700536de89afc7ec6289a2aefbc8c7
OM BURSE <omburse0019@gmail.com>
2026-04-12 17:22:23 +0530

    Used For and While loop

---
+++ Loops.java
@@ -0,0 +1,67 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Loops {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //--For loop--
+
+        /*
+            //Print Square Pattern
+              ****
+              ****
+              ****
+              ****
+         */
+//        for(int i=1;i<=4;i++)
+//        {
+//            System.out.println("****");
+//        }
+//        System.out.println("Exit for loop");
+
+
+        // Print a Revers of Numbers
+
+        // N = 19112004 Revers 40021191
+//            int N = 19112004;
+//            int rev = 0;
+//             while (N > 0){
+//                 int LastDigit = N%10;
+//                 rev = (rev * 10)+LastDigit;
+//                 N = N/10;
+//             }
+//            System.out.print( rev + " ");
+
+
+
+        //--While Loop--
+        // Print While loop are 1 to 10
+//        int i=1;
+//        while (i<=10){
+//            System.out.println(i);
+//            i++;
+//        }
+
+        // Print No 1 to N Using User Input
+//        System.out.print("Enter Number : ");
+//        int Range = sc.nextInt();
+//        int i = 1;
+//
+//        while (i<=Range){
+//            System.out.println(i + " ");
+//            i++;
+//        }
+
+
+        // Print Sum of First N Natural Number
+//        System.out.print("Enter The Number : ");
+//        int n = sc.nextInt();
+//        int sum = 0;
+//        int i = 1;
+//        while (i<=n){
+//            sum = sum + i;
+//            i++;
+//        }
+//        System.out.println("The Sum Is : " + sum);
+        
+    }
+}
