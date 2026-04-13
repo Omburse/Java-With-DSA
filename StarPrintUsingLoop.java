9b3cbfe21f7d6ec5f144d64bdca79ba60e6cb718
OM BURSE <omburse0019@gmail.com>
2026-04-13 08:45:32 +0530

    Simple Login In Nested Loop

---
+++ StarPrintUsingLoop.java
@@ -0,0 +1,66 @@
+public class StarPrintUsingLoop {
+    public static void main(String [] args){
+        /*
+            *
+            * *
+            * * *
+            * * * *
+         */
+//        for(int i=1;i<=4;i++){
+//            for(int j=1;j<=i;j++){
+//                System.out.print("*" + " ");
+//            }
+//            System.out.println(" ");
+//        }
+
+
+
+        // Print Inverted Star
+        /*
+            * * * *
+            * * *
+            * *
+            *
+            Formula = n-i+1
+         */
+//        for(int i=1;i<=4;i++){
+//            for(int j=1;j<=4-i+1;j++){
+//                System.out.print("*" + " ");
+//            }
+//            System.out.println();
+//        }
+
+
+
+        // Print HALF-PYRAMID Pattern
+        /*
+            1
+            1 2
+            1 2 3
+            1 2 3 4
+         */
+
+//        for(int i=1;i<=4;i++){
+//            for(int j=1;j<=i;j++){
+//                System.out.print(j + " ");
+//            }
+//            System.out.println();
+//        }
+
+        // Print Characters
+        /*
+            A
+            B C
+            D E F
+            G H I J
+         */
+//        char ch = 'A';
+//        for(char A=1;A<=4;A++){
+//            for(char j=1;j<=A;j++){
+//                System.out.print(ch + " ");
+//                ch++;
+//            }
+//            System.out.println();
+//        }
+    }
+}
